b005ba278675d196d2865739f2273800f1280ad5
Ryan Thomas Huff <rhuff@byteworks.com>
2018-12-05 12:18:18 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     
-    <title>NetGain IM Compliance Reporting Tool</title>
+    <title>IM Compliance Reporting Tool</title>
     <LINK href="css/mainNavigation.css" rel="stylesheet" type="text/css">
   </head>
     <body style="font-family: Tahoma, Verdana, Arial;">
